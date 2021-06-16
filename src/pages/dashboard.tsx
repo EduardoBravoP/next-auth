5d03b27f8cda6555beb2410e5e7c7f5403d69531
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-16 14:26:52 -0300

    commit

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,18 @@
-import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
-import { AuthTokenError } from "../services/errors/AuthTokenError"
 import { withSSRAuth } from "../utils/withSSRAuth"
+import { useCan } from '../hooks/useCan'
 
 export default function Dashboard() {
   const {user} = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
+
+  console.log('oi')
   
   useEffect(() => {
     api.get('/me')
@@ -16,7 +21,11 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
   )
 }
 
